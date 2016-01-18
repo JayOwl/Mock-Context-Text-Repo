aa14d1bf8319a2e6b4c10ead3afa6c97a8fd0339
Joel Richard Benoit <joel.r.benoit@gmail.com>
2016-01-18 11:24:53 -0800

    Test Driven Development (TDD) .NET MVC
    Automated testing can help ensure that your project behaviour falls
    within expected boundaries.  Including testing in extremely large
    projects can help eliminate vulnerabilities with automated checks for
    expected responses under specific conditions.
    Testing is sometimes encouraged during development – this practice is
    called Test Driven Development (TDD).  TDD is useful since relevant
    tests can be added while the developer is focused on the module.  The
    danger of TDD is too much time can be spent writing tests on code that
    is eventually replaced or thrown out.
    Regardless of whether or not automated testing is useful for your
    project, it is important to be aware of how to test so you can make a
    better decision as to when and how you wish to test your code.

---
+++ MockContextTestRepoClass/Repo/ManufacturerRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MockContextTestRepoClass.Repo
+{
+    public class ManufacturerRepository
+    {
+        // The constructor allows us to pass in the mock context.
+        private JB_FoodStoreEntities context = new JB_FoodStoreEntities();
+        public ManufacturerRepository(JB_FoodStoreEntities context)
+        {
+            this.context = context;
+        }
+
+        public List<Manufacturer> GetAllManufacturers()
+        {
+            return context.Manufacturers.ToList();
+        }
+
+        public Manufacturer GetManufacturer(string mfg)
+        {
+            return context.Manufacturers.Where(m => m.mfg == mfg)
+                          .FirstOrDefault();
+        }
+
+    }
+}
---
+++ MockContextTestRepoClass/Repo/ProductRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MockContextTestRepoClass.Repo
+{
+    public class ProductRepository
+    {
+        // The constructor allows us to pass in the mock context.
+        private JB_FoodStoreEntities context = new JB_FoodStoreEntities();
+        public ProductRepository(JB_FoodStoreEntities context)
+        {
+            this.context = context;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return context.Products.ToList();
+        }
+
+        public Product GetProduct(int productID)
+        {
+            return context.Products.Where(p => p.productID == productID)
+                          .FirstOrDefault();
+        }
+
+    }
+}
